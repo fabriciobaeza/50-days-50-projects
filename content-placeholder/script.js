33cec99006ac6db65113ee5d78306255f5196d13
Fabricio Baeza <fabriciobaeza@gmail.com>
2021-03-10 21:55:04 -0500

    Animation Timeout Effect

---
+++ content-placeholder/script.js
@@ -0,0 +1,31 @@
+const header = document.getElementById("header");
+const title = document.getElementById("title");
+const excerpt = document.getElementById("excerpt");
+const profile_img = document.getElementById("profile_img");
+const name = document.getElementById("name");
+const date = document.getElementById("date");
+
+const animated_bgs = document.querySelectorAll(".animated-bg");
+const animated_bg_texts = document.querySelectorAll(".animated-bg-text");
+
+setTimeout(getData, 2500)
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/the-mandalorian-season-2-1604051753.jpeg" alt="" />';
+
+  title.innerHTML = "Lorem ipsum dolor sit amet.";
+
+  excerpt.innerHTML =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae,odit?";
+
+  profile_img.innerHTML =
+    '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="">';
+
+  name.innerHTML = "John Doe";
+
+  date.innerHTML = "March 10, 2021";
+
+  animated_bgs.forEach((bg) => bg.classList.remove("animated-bg"));
+  animated_bg_texts.forEach((bg) => bg.classList.remove("animated-bg-text"));
+}
