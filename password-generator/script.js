3fed48f52b98021f796d0828a1be31f30fede085
Fabricio Baeza <fabriciobaeza@gmail.com>
2021-03-19 23:20:05 -0400

    Copy Password To Clipboard

--- password-generator/script.js
+++ password-generator/script.js
@@ -14,8 +14,24 @@ const randomFunc = {
   symbol: getRandomSymbol,
 };
 
+clipboardEl.addEventListener("click", () => {
+  const textarea = document.createElement("textarea");
+  const password = resultEl.innerText;
+
+  if (!password) {
+    return;
+  }
+
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  textarea.remove();
+  alert("Password copied to clipboard!");
+});
+
 generateEl.addEventListener("click", () => {
-  const length = +lengthEl.length;
+  const length = +lengthEl.value;
   const hasLower = lowercaseEl.checked;
   const hasUpper = uppercaseEl.checked;
   const hasNumber = numbersEl.checked;
